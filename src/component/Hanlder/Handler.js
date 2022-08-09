6c9b6ecbad7b368ce42d1a5ff6fd22100c435a94
tuanphamdvf <104239862+tuanphamdvf@users.noreply.github.com>
2022-08-09 16:24:22 +0700

    [UPDATE]--Filter, Edit product

--- src/component/Hanlder/Handler.js
+++ src/component/Hanlder/Handler.js
@@ -37,7 +37,6 @@ function reloadRouter() {
         window.location.reload();
     }, 0);
 }
-
 //get element route
 const titleDashboard = document.getElementById('title__dashbroad');
 const titleProduct = document.getElementById('title__product');
@@ -86,14 +85,10 @@ window.addEventListener('load', function () {
     const elementProduct = document.getElementById('product__id');
     const oderElement = document.querySelector('.table3--wrapper--item');
     const productElement = document.querySelector('.table2--wrapper--item');
-    const elementPageSize = document.querySelector('.paging--short--number');
-    const prePage = document.querySelector('.product--prePage');
-    const nextPage = document.querySelector('.product--nextPage');
-    const contentPageCount = document.getElementById('countpage');
-    const loader = document.querySelector('.error--page');
-    const submitAddProduct = document.getElementById('create--product');
     const elementAddProduct = document.getElementById('addproduct__id');
     const elementRemoveProduct = document.querySelector('.popup--delete');
+    const elementFilterProdcut = document.getElementById('product--filter__id');
+    const searchProductElement = document.querySelector('.product--search--input');
 
     // handle render page
     if (elementProduct) {
@@ -102,74 +97,8 @@ window.addEventListener('load', function () {
                 return response.json();
             })
             .then(function (productItem) {
-                //handle pagging here
-                // let totalProduct = productItem.length; // tổng số sản phẩm
-                // let page_size = 10; // số sản phẩm 1 trang mặc định
-                // let page_number = 1; // trang hiện tại mặc định
-                // let fromItemNumber = 1; // giá trị bắt đầu của trang hiện tại mặc định
-                // let toItemNumber = 10; // giá trị kết thúc của trang hiện tại mặc định
-                // if (page_size > totalProduct) {
-                //     toItemNumber = totalProduct;
-                // }
-                // let numberPage = countPage(productItem, page_size); // số page
-                // // the previous page
-                // prePage.onclick = function () {
-                //     //Obtain the current value and handle
-                //     if (page_number > 1) {
-                //         fromItemNumber = (page_number - 2) * parseInt(page_size) + 1;
-                //         toItemNumber = fromItemNumber + parseInt(page_size) - 1;
-                //         page_number = page_number - 1;
-                //         const newArrayProduct = paginate(productItem, page_size, page_number);
-                //         elementProduct.innerHTML = '';
-                //         contentPageCount.innerHTML = `${fromItemNumber}- ${toItemNumber} of ${totalProduct}`;
-                //         renderProducttable(elementProduct, newArrayProduct);
-                //     }
-                // };
-                // // manage page size change
-                // elementPageSize.onchange = function () {
-                //     //Obtain the current value and handle
-                //     page_size = parseInt(elementPageSize.value); //
-                //     numberPage = countPage(productItem, page_size); //
-                //     fromItemNumber = (page_number - 1) * parseInt(page_size) + 1;
-                //     toItemNumber = fromItemNumber + parseInt(page_size) - 1;
-
-                //     elementProduct.innerHTML = '';
-                //     // handle when you get to the last page
-                //     if (totalProduct < page_size * page_number) {
-                //         numberPage = countPage(productItem, page_size);
-                //         page_number = Math.ceil(totalProduct / page_size);
-                //         fromItemNumber = (page_number - 1) * parseInt(page_size) + 1;
-                //     }
-                //     if (toItemNumber > totalProduct) {
-                //         toItemNumber = totalProduct;
-                //     }
-                //     const newArrayProduct = paginate(productItem, page_size, page_number);
-                //     renderProducttable(elementProduct, newArrayProduct);
-                //     contentPageCount.innerHTML = `${fromItemNumber}- ${toItemNumber} of ${totalProduct}`;
-                // };
-                // //next to the page
-                // nextPage.onclick = function () {
-                //     if (page_number < numberPage) {
-                //         //Obtain the current value and handle
-                //         fromItemNumber = page_number * parseInt(page_size) + 1;
-                //         toItemNumber = fromItemNumber + parseInt(page_size) - 1;
-                //         page_number = page_number + 1;
-                //         // handle when you get to the last page
-                //         if (toItemNumber > totalProduct) {
-                //             toItemNumber = totalProduct;
-                //         }
-                //         const newArrayProduct = paginate(productItem, page_size, page_number);
-                //         elementProduct.innerHTML = '';
-
-                //         contentPageCount.innerHTML = `${fromItemNumber}- ${toItemNumber} of ${totalProduct}`;
-                //         renderProducttable(elementProduct, newArrayProduct);
-                //     }
-                // };
-                const newArrayProduct = paginate(productItem, page_size, page_number);
-                contentPageCount.innerHTML = `${fromItemNumber}- ${toItemNumber} of ${totalProduct}`;
-                renderProducttable(elementProduct, newArrayProduct);
+                paging(elementProduct, productItem);
             });
-
         document.querySelector('.product--button--add').addEventListener('click', reloadRouter);
         document.querySelector('.product--button--filter').addEventListener('click', reloadRouter);
     } else if (elementNewCustomer) {
@@ -180,17 +109,31 @@ window.addEventListener('load', function () {
         renderAddProduct(elementAddProduct);
         const elementDate = document.querySelector('.addproduct--input--date');
         elementDate.value = today;
-
+        // Validate from add Product
         const saveProductButton = document.getElementById('create--product');
-        saveProductButton.addEventListener('click', () => {
-            // reloadRouter();
-
-            handleOnclickAddproduct();
-        });
+        const nameProductNew = document.getElementById('addproduct__name');
+        const elementWaring = document.querySelector('.addproduct--name--warning');
+        nameProductNew.onblur = () => {
+            if (nameProductNew.value == '') {
+                elementWaring.style.display = 'block';
+            } else {
+                elementWaring.style.display = 'none';
+                saveProductButton.href = '/#product';
+                saveProductButton.addEventListener('click', () => {
+                    // reloadRouter();
+                    handleOnclickAddproduct();
+                });
+            }
+        };
+        nameProductNew.onfocus = () => {
+            elementWaring.style.display = 'none';
+        };
         const backProductButton = document.getElementById('back--addproduct');
         backProductButton.addEventListener('click', reloadRouter);
     } else if (elementRemoveProduct) {
-        console.log(elementRemoveProduct);
+    } else if (elementFilterProdcut) {
+        const elementButtonHideFilter = document.querySelector('.product--button--filter');
+        elementButtonHideFilter.addEventListener('click', reloadRouter);
     } else {
         const html = (document.getElementById('loader').style.display = 'flex');
         window.location.reload();
--- src/component/product/handleProduct.js
+++ src/component/product/handleProduct.js
@@ -1,10 +1,10 @@
 // RENDER PRODUCT
 function renderAddProduct(elementAdd) {
-    elementAdd.innerHTML = `<div class="addproduct--header">
+    elementAdd.innerHTML = `<div id ="from-1" class="addproduct--header">
         <span class="addproduct--header--title">Thêm mới sản phẩm
         </span>
         <div class="addproduct--action">
-            <a href="/#product" class="addproduct--save" id="create--product">Lưu lại</a>
+            <a class="addproduct--save" id="create--product">Lưu lại</a>
             <a href="/#product" class="addproduct--back" id ="back--addproduct">Quay lại</a>
         </div>
     </div>
@@ -12,12 +12,13 @@ function renderAddProduct(elementAdd) {
         <div class="addproduct--content">
             <div class="addproduct--name">
                 <input class="addproduct--input--name" placeholder=" Tên sản phẩm" id="addproduct__name" />
+                <span class = "addproduct--name--warning">Bạn cần nhập tên sản phẩm !</span>
             </div>
-            <div class="addproduct--number"> <input placeholder="cái"  class="addproduct--input--number"
+            <div class="addproduct--number"> <input type="number" placeholder="cái"  class="addproduct--input--number"
                     id="addproduct__number" /><span class="addproduct--number--after">Số lượng</span></div>
             <div class="addproduct--group">
                 <select class="addproduct--input--group" id="addproduct__group">
-                    <option value="" disabled selected hidden>Loại SP</option>
+                    <option value="Mới tạo" disabled selected hidden>Loại SP</option>
                     <option>Chuột </option>
                     <option>Bàn phím</option>
                     <option>Ghế công thái học</option>
@@ -34,27 +35,12 @@ function renderAddProduct(elementAdd) {
             </div>
             <div class="addproduct--date"> <input type="date" class="addproduct--input--date" /><span
                     class="addproduct--number--date" id="addproduct__date">Ngày tạo </span></div>
-            <div class="addproduct--price"> <input placeholder="VND" class="addproduct--input--price"
-                    id="addproduct__price"/><span class="addproduct--number--price">Giá bán</span></div>
+            <div class="addproduct--price"> <input type="number" placeholder="VND" class="addproduct--input--price"
+                    id="addproduct__price"/><span  class="addproduct--number--price">Giá bán</span></div>
         </div>
     </div>`;
 }
 // ---- ADD PRODUCT ----
-
-// // Validate -from
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Mong muốn của chúng ta
-//     Validator({
-//         form: '#form-1',
-//         formGroupSelector: '.form-group',
-//         errorSelector: '.form-message',
-//         rules: [Validator.isRequired('#fullname', 'Vui lòng nhập tên đầy đủ của bạn')],
-//         onSubmit: function (data) {
-//             // Call API
-//             console.log(data);
-//         },
-//     });
-// });
 //Function - add one product to the last array
 function addProductJson(data, callback) {
     let options = {
@@ -70,24 +56,38 @@ function addProductJson(data, callback) {
         })
         .then(callback);
 }
+
 // Function -obtain input product value
 function handleOnclickAddproduct() {
     const nameNewProduct = document.getElementById('addproduct__name').value;
-    const numberNewProduct = document.getElementById('addproduct__number').value;
     const groupNewProduct = document.getElementById('addproduct__group').value;
-    const statusNewProduct = document.getElementById('addproduct__status').value;
-    const priceNewProduct = document.getElementById('addproduct__price').value;
     const elementDateValue = document.querySelector('.addproduct--input--date').value;
-    if (numberNewProduct == '') {
+    let valuePrice = 0;
+    let valueNumber = 0;
+    let status = 'Hết hàng';
+    //Check input
+    const numberNewProduct = document.getElementById('addproduct__number');
+    if (numberNewProduct.value != '') {
+        valueNumber = numberNewProduct.value;
+        status = 'Còn hàng';
+    }
+    const priceNewProduct = document.getElementById('addproduct__price');
+    if (priceNewProduct.value != '') {
+        valuePrice = priceNewProduct.value;
     }
     let itemData = {
         name: nameNewProduct,
-        number: numberNewProduct,
+        number: valueNumber,
         createDate: elementDateValue,
         group: groupNewProduct,
-        price: priceNewProduct,
+        price: valuePrice,
+        status: status,
     };
-    // addProductJson(itemData);
+    if (!nameNewProduct) {
+        elementWaring.style.display = 'block';
+    } else {
+        addProductJson(itemData);
+    }
 }
 //---DELETE PRODUCT---
 //function:  When using onclick, remove one product.
@@ -110,4 +110,111 @@ function showPopupDelete(id) {
 function hidePopupDeleteProduct(id) {
     const buttonHandleDelete = document.querySelector('.popup--delete--' + id);
     buttonHandleDelete.style.display = 'none';
+}
+//--FILLTER--
+
+window.addEventListener('load', () => {
+    const elementFilterProdcut = document.getElementById('product--filter__id');
+    const filterValue = document.getElementById('filter__id');
+
+    if (filterValue) {
+        fetch(productApi)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (productItem) {
+                // renderProducttable(elementFilterProdcut, productItem);
+                paging(elementFilterProdcut, productItem);
+            });
+        filterValue.addEventListener('input', filterGoods);
+        function filterGoods() {
+            let filterName = document.querySelector('#filter__name').value;
+            let filterGroup = filterValue.querySelector('#filter__group').value;
+            let filterNumber = filterValue.querySelector('#filter__number').value;
+            let filterStatus = filterValue.querySelector('#filter__status').value;
+            let nameNew = removeVietnameseTones(filterName);
+
+            fetch(productApi)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (productItem) {
+                    let newArrayFilter = productItem.filter((n) => {
+                        let nameProductDatabase = removeVietnameseTones(n.name);
+                        return (
+                            (!filterGroup || n.group == filterGroup) &&
+                            (!filterNumber || n.number == filterNumber) &&
+                            (!filterStatus || n.status == filterStatus) &&
+                            (!nameNew || nameProductDatabase == nameNew)
+                        );
+                    });
+                    elementFilterProdcut.innerHTML = '';
+                    paging(elementFilterProdcut, newArrayFilter);
+                });
+        }
+    }
+});
+//--- EDIT ---
+//Show edit product, get value and handler edit
+function showPopupEdit(id) {
+    const buttonHandleEdit = document.querySelector('.popup--edit--' + id);
+    document.querySelector('.wrapper--paging').style.display = 'none';
+    buttonHandleEdit.style.display = 'flex';
+    let editValue = document.getElementById('edit__id__' + id);
+    const buttonEditSave = document.querySelector('#button__edit__save__' + id);
+    console.log(editValue);
+    if (editValue) {
+        editValue.addEventListener('change', editFunction);
+        function editFunction() {
+            let editName = editValue.querySelector('#edit__product__name').value;
+            let editGroup = editValue.querySelector('#edit__product__group').value;
+            let editDate = editValue.querySelector('#edit__product__date').value;
+            let numberEditProduct = editValue.querySelector('#edit__product__number').value;
+            let priceEditProduct = editValue.querySelector('#edit__product__price').value;
+            let valuePriceEdit = 0;
+            let valueNumberEdit = 0;
+            let statusEdit = 'Hết hàng';
+            //check value input
+            if (numberEditProduct != '') {
+                valueNumberEdit = numberEditProduct;
+                statusEdit = 'Còn hàng';
+            }
+            if (priceEditProduct != '') {
+                valuePriceEdit = priceEditProduct;
+            }
+            let editData = {
+                name: editName,
+                number: valueNumberEdit,
+                createDate: editDate,
+                group: editGroup,
+                price: valuePriceEdit,
+                status: statusEdit,
+            };
+            buttonEditSave.addEventListener('click', () => {
+                editProductJson(editData, id);
+            });
+        }
+        buttonEditSave.addEventListener('click', () => {
+            hidePopupEditProduct(id);
+        });
+    }
+}
+
+function hidePopupEditProduct(id) {
+    const buttonHandleEdit = document.querySelector('.popup--edit--' + id);
+    document.querySelector('.wrapper--paging').style.display = 'flex';
+    buttonHandleEdit.style.display = 'none';
+}
+// API EDIT JSON
+function editProductJson(data, id) {
+    var option = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    fetch(productApi + '/' + id, option).then(function (response) {
+        return response;
+    });
 }
